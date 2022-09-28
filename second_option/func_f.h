e5ab51b29add4a5aa53b95b07c9b396ab4b49e55
Danylo <sydorenko2020ks15@student.karazin.ua>
2022-09-28 22:40:48 +0300

    Жєсть

--- second_option/func_f.h
+++
@@ -1,6 +0,0 @@
-#include<math.h>
-#include<stdlib.h>
-
-double f(double x) {
-        return  exp(-1 * fabs(x)) * sin(x);
-}
--- second_option/func_g.h
+++
@@ -1,6 +0,0 @@
-#include<math.h>
-#include<stdlib.h>
-
-double g(double x) {
-        return exp(-1 * fabs(x)) * cos(x);
-}
